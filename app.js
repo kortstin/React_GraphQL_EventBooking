9d9ac2b6145214f5e883e636e4cfc925c662026f
Kortney Stinson <kortneystinson@gmail.com>
2021-02-06 14:54:33 -0500

    Enhance MongoDB relations in GraphQL API, dynamic resolvers, nested relations, and change to file structure with separate resolver/schema folders

--- app.js
+++ app.js
@@ -2,12 +2,10 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const { graphqlHTTP } = require('express-graphql');
-const { buildSchema } = require('graphql');
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
 
-const Event = require('./models/event');
-const User = require('./models/user');
+const graphqlSchema = require('./graphql/schema/index');
+const graphqlResolvers = require('./graphql/resolvers/index');
 
 const app = express();
 
@@ -15,114 +13,11 @@ const uri = process.env.ATLAS_URI;
 
 app.use(bodyParser.json());
 
+
+
 app.use('/graphql', graphqlHTTP({
-    schema: buildSchema(`
-        type Event {
-            _id: ID!
-            title: String!
-            description: String!
-            price: Float!
-            date: String!
-        }
-
-        type User {
-            _id: ID!
-            email: String!
-            password: String
-            
-        }
-
-        input EventInput {
-            title: String! 
-            description: String!
-            price: Float!
-            date: String!
-        }
-
-        input UserInput {
-            email: String!
-            password: String!
-        }
-
-        type RootQuery {
-            events: [Event!]!
-        }
-
-        type RootMutation {
-            createEvent(eventInput: EventInput): Event
-            createUser(userInput: UserInput): User
-        }
-
-        schema {
-            query: RootQuery
-            mutation: RootMutation
-        }
-    `),
-    rootValue: {
-        events: async () => {
-         try {
-                const events = await Event.find();
-                return events.map(event => {
-                    return { ...event._doc };
-                });
-            } catch (err) {
-                throw err;
-            }
-        },
-        createEvent: args => {
-            const event = new Event({
-                title: args.eventInput.title,
-                description: args.eventInput.description,
-                price: +args.eventInput.price,
-                date: new Date(args.eventInput.date),
-                creator: '601e17faf75e4675f4f4339b'
-            });
-            let createdEvent;
-            return event
-                .save()
-                .then(result => {
-                    createdEvent = { ...result._doc, _id: result.id };
-                    return User.findById('601e17faf75e4675f4f4339b');  
-                })
-                .then(user => {
-                    if(!user) {
-                        throw new Error("User not found.");
-                    }
-                    user.createdEvents.push(event);
-                    return user.save();
-                })
-                .then(result => {
-                    return createdEvent;
-                })
-                .catch(err => {
-                    console.log(err);
-                    throw err;
-                });
-           
-        },
-        createUser: args => {
-            return User.findOne({email:args.userInput.email}).then(user => {
-                if(user) {
-                    throw new Error("User already exists.");
-                }
-                return bcrypt.hash(args.userInput.password, 12)
-            })
-            .then(hashedPassword => {
-                const user = new User({
-                    email: args.userInput.email,
-                    password: hashedPassword
-                });
-                return user.save();
-            })
-            .then(result => {
-                return { ...result._doc, password: null, _id: result.id };
-            })
-            .catch(err => {
-                throw err;
-            });
-            
-        }
-    },
+    schema: graphqlSchema,
+    rootValue: graphqlResolvers,
     graphiql: true
 }));
 
